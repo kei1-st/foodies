f850a6f4c34f8ea8e31b7353078efe1fb841c114
kei1_st <kei1satoh0603@gmail.com>
2024-02-19 23:03:31 +0900

    appy css

--- components/main-header.js
+++ components/main-header.js
@@ -1,15 +1,16 @@
 import Link from "next/link";
 
 import logoImg from "@/assets/logo.png";
+import classes from "@/components/main-header.module.css";
 
 export default function MainHeader() {
   return (
-    <header>
-      <Link href="/">
+    <header className={classes.header}>
+      <Link className={classes.logo} href="/">  {/* スタイルをコンポーネントに直接適用する */}
         <img src={logoImg.src} alt="a plate with food on it" />
         Next Level Food
       </Link>
-      <nav>
+      <nav className={classes.nav}>
         <ul>
           <li>
             <Link href="/meals">Browse Meals</Link>
