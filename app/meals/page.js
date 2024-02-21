012b13ac86d2ffed0dfef44800031449b9ea351f
kei1_st <kei1satoh0603@gmail.com>
2024-02-21 22:16:45 +0900

    add content to MealsPage

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,15 +1,22 @@
+import MealsGrid from "@/components/meals/meals-grid";
+import classes from "./page.module.css";
 import Link from "next/link";
 
 export default function MealsPage() {
   return (
-    <main>
-      <h1>Meals Page</h1>
-      <p>
-        <Link href="meals/post-1">Post 1</Link>
-      </p>
-      <p>
-        <Link href="meals/post-2">Post 2</Link>
-      </p>
-    </main>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Deliscious meals, created <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>Choose your favorite recipe and cook it yourself. It is easy and fun!!</p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main>
+        <MealsGrid meals={[]}/>
+      </main>
+    </>
   );
 }
