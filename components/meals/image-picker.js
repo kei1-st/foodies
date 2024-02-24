18b6604163326476381f5c22a93add032182fcd3
kei1_st <kei1satoh0603@gmail.com>
2024-02-24 15:48:56 +0900

    選択した画像のプレビューを追加

--- components/meals/image-picker.js
+++ components/meals/image-picker.js
@@ -1,23 +1,50 @@
-'use client';
+"use client";
 
-import { useRef } from "react";
+import { useRef, useState } from "react";
+import Image from "next/image";
 
 import classes from "./image-picker.module.css";
 
 export default function ImagePicker({ label, name }) {
-
+  const [pickedImage, setPickedImage] = useState();
   const imageInput = useRef();
 
-  function handlePickClick(){
+  function handlePickClick() {
     imageInput.current.click();
+  }
+
+  function handleImageChange(event) {
+    const file = event.target.files[0];
+    if (!file) {
+      return;
+    }
+    const fileReader = new FileReader();
+    fileReader.onload = (url) => {
+      setPickedImage(fileReader.result);
+    };
+    fileReader.readAsDataURL(file);
   }
 
   return (
     <div className={classes.picker}>
       <label htmlFor={name}>{label}</label> {/* id === image のフォームの入力 */}
       <div className={classes.control}>
-        <input className={classes.input} type="file" id={name} accept=".jpg,.png,.jpeg" name={name} ref={imageInput}/>
-        <button className={classes.button} type="button" onClick={handlePickClick}>Pick An Image</button>
+        <div className={classes.preview}>
+          {!pickedImage && <p>No image picked.</p>}
+          {pickedImage && <Image src={pickedImage} alt="The image selected by the user." fill />}
+        </div>
+        <input
+          className={classes.input}
+          type="file"
+          id={name}
+          accept=".jpg,.png,.jpeg"
+          name={name}
+          ref={imageInput}
+          onChange={handleImageChange}
+        />
+        <button className={classes.button} type="button" onClick={handlePickClick}>
+          Pick An Image
+        </button>
       </div>
     </div>
   );
