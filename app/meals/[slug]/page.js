0a667cde277e0d586ee58982695c723fd2b8e85c
kei1_st <kei1satoh0603@gmail.com>
2024-02-24 15:17:48 +0900

    notFound() の呼び出しを追加とフォーマット

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,13 +1,18 @@
 import Image from "next/image";
+import { notFound } from "next/navigation";
 
 import { getMeal } from "@/lib/meals";
 import classes from "./page.module.css";
 
-export default function MealDetailsPage({ params }) { //params = {slug: '...'}
+export default function MealDetailsPage({ params }) {
+  //params = {slug: '...'}
   // params は nextjs の仕様
   const meal = getMeal(params.slug);
+  if (!meal) {
+    notFound(); // ディレクトリ内で最も近い not-found.js を呼び出す
+  }
 
-  meal.instructions= meal.instructions.replace(/\n/g, '<br />');
+  meal.instructions = meal.instructions.replace(/\n/g, "<br />");
 
   return (
     <>
@@ -24,9 +29,13 @@ export default function MealDetailsPage({ params }) { //params = {slug: '...'}
         </div>
       </header>
       <main>
-        <p className={classes.instructions} dangerouslySetInnerHTML={{
-          __html: meal.instructions,
-        }}></p>  {/* クロスサイトスクリプティング対策 */}
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        ></p>{" "}
+        {/* クロスサイトスクリプティング対策 */}
       </main>
     </>
   );
