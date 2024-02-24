2e4b80b0f66d444cc15a5710de692587ae51883d
kei1_st <kei1satoh0603@gmail.com>
2024-02-24 15:40:42 +0900

    ファイル選択ボタンを独自ボタンに変更

--- components/meals/image-picker.js
+++ components/meals/image-picker.js
@@ -1,11 +1,23 @@
+'use client';
+
+import { useRef } from "react";
+
 import classes from "./image-picker.module.css";
 
 export default function ImagePicker({ label, name }) {
+
+  const imageInput = useRef();
+
+  function handlePickClick(){
+    imageInput.current.click();
+  }
+
   return (
     <div className={classes.picker}>
       <label htmlFor={name}>{label}</label> {/* id === image のフォームの入力 */}
       <div className={classes.control}>
-        <input type="file" id={name} accept=".jpg,.png,.jpeg" name={name} />
+        <input className={classes.input} type="file" id={name} accept=".jpg,.png,.jpeg" name={name} ref={imageInput}/>
+        <button className={classes.button} type="button" onClick={handlePickClick}>Pick An Image</button>
       </div>
     </div>
   );
