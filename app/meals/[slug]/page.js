9c2ca170899d568b3fae88e2fba6196bcb376852
kei1_st <kei1satoh0603@gmail.com>
2024-02-24 15:06:44 +0900

    add contents to MealDetailsPage

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,9 +1,33 @@
-export default function MealDetailsPage({ params }) {
+import Image from "next/image";
+
+import { getMeal } from "@/lib/meals";
+import classes from "./page.module.css";
+
+export default function MealDetailsPage({ params }) { //params = {slug: '...'}
   // params は nextjs の仕様
+  const meal = getMeal(params.slug);
+
+  meal.instructions= meal.instructions.replace(/\n/g, '<br />');
+
   return (
-    <main>
-      <h1>Meal Details</h1>
-      <p>{params.slug}</p> {/* ページに対して一意 */}
-    </main>
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p className={classes.instructions} dangerouslySetInnerHTML={{
+          __html: meal.instructions,
+        }}></p>  {/* クロスサイトスクリプティング対策 */}
+      </main>
+    </>
   );
 }
